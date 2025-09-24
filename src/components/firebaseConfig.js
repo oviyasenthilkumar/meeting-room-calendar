37e0b0397f7b3db5e35f89d6f0a58a34004b8986
oviyasenthilkumar <154113657+oviyasenthilkumar@users.noreply.github.com>
2025-09-24 17:07:59 +0530

    Update firebaseConfig.js

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -5,25 +5,25 @@ import { getAuth } from "firebase/auth";
 
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDOJCBuO4t2GMfgJughurnj9LVzvJwToJM",
-//   authDomain: "meeting-room-booking-c26be.firebaseapp.com",
-//   projectId: "meeting-room-booking-c26be",
-//   storageBucket: "meeting-room-booking-c26be.firebasestorage.app",
-//   messagingSenderId: "932152712614",
-//   appId: "1:932152712614:web:3cd867ffbf45d351ae4367",
-//   measurementId: "G-70HBFLNLQW",
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyD27iKf5mJ7_b5FH7lF57lSf-8yQf7ZqEU",
-  authDomain: "meetlink-2a1e0.firebaseapp.com",
-  projectId: "meetlink-2a1e0",
-  storageBucket: "meetlink-2a1e0.firebasestorage.app",
-  messagingSenderId: "931083093340",
-  appId: "1:931083093340:web:5053ce1db2b7cda5e06d99",
-  measurementId: "G-BFDZN5HYKK"
+  apiKey: "AIzaSyDOJCBuO4t2GMfgJughurnj9LVzvJwToJM",
+  authDomain: "meeting-room-booking-c26be.firebaseapp.com",
+  projectId: "meeting-room-booking-c26be",
+  storageBucket: "meeting-room-booking-c26be.firebasestorage.app",
+  messagingSenderId: "932152712614",
+  appId: "1:932152712614:web:3cd867ffbf45d351ae4367",
+  measurementId: "G-70HBFLNLQW",
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD27iKf5mJ7_b5FH7lF57lSf-8yQf7ZqEU",
+//   authDomain: "meetlink-2a1e0.firebaseapp.com",
+//   projectId: "meetlink-2a1e0",
+//   storageBucket: "meetlink-2a1e0.firebasestorage.app",
+//   messagingSenderId: "931083093340",
+//   appId: "1:931083093340:web:5053ce1db2b7cda5e06d99",
+//   measurementId: "G-BFDZN5HYKK"
+// };
 
 // const firebaseConfig = {
 //   apiKey: "AIzaSyDxsNmA39jpHc-b6XC9LyZtNJdvhT5Aki8",
